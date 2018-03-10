9892a9e3931ceffdb2eadb489019b27addce6716
Deilan <deilan@mail.ru>
2018-03-10 22:46:56 +0300

    temp

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,30 @@
-console.log('Hello world!');
+var container = document.querySelector('#container');
+var template = document.querySelector('#template').content;
+var itemTemplate = template.querySelector('.Item');
 
-// var template = document.querySelector('#template').innerHTML;
+var fragment = document.createDocumentFragment();
+window.data.forEach(function(item) {
+  var node = itemTemplate.cloneNode(true);
+  node.querySelector('.Item-Title').textContent = item.title;
+  node.querySelector('.Item-Image img').src = item.image;
+  if(item.description) {
+    var descriptionFragment = getDescriptionFragment(item.description);
+    node.querySelector('.Item-Description').appendChild(descriptionFragment);
+  } else {
+    var descriptionEl = node.querySelector('.Item-Description');
+    descriptionEl.parentNode.removeChild(descriptionEl);
+  } 
 
-// var compiledTemplate = Handlebars.compile(quoteInfo);
+  fragment.appendChild(node);
+})
+container.appendChild(fragment);
 
-// var quoteData = template({
-//   title: "123",
-//   imageUrl: "http://ya.ru",
-//   content: "wololo"
-// })
+function getDescriptionFragment(description) {
+  var fragment = document.createDocumentFragment();
+  description.split('\n').forEach(function(paragraph){
+    var paragraphEl = document.createElement('p');
+    paragraphEl.textContent = paragraph;
+    fragment.appendChild(paragraphEl);
+  });
+  return fragment;
+}
